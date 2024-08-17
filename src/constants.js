e053ec42bce9527fef7c41af828410874e8c6b86
Nikhil <140883532+NikhilSaharan@users.noreply.github.com>
2024-08-17 18:52:55 +0530

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,65 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Swimmer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Nikhil Kumar';
+
+export const experiences = [
+    {
+        'company': 'SoftSynth Software Solutions',
+        'role': 'Frontend Developer',
+        'duration': 'Feb 2024 - Present',
+        'logo': 'https://media.licdn.com/dms/image/C560BAQGZ1BmJAedR2A/company-logo_200_200/0/1639371530480?e=2147483647&v=beta&t=4J_qLy7bFdEnP6y3buD2eJgAYb65obLrsnfXozhlFE8',
+        'logoSrc': 'https://images.app.goo.gl/68wGowkGLWcY5y986', 
+        'points': [
+            ' Collaborate with the project team to understand the clients needs and objectives for the website. Gather requirements related to design, functionality, and user experience',
+            ' As a front-end developer, I specialize in React.js, delivering dynamic and user-centric web interfaces that exceed client expectations With a proven track record of collaborating seamlessly with cross-functional teams, I ensure optimal integration of front-end solutions with back-end systems. My commitment to quality, attention to detail, and effective communication skills make me avaluable asset in driving project success and maintaining client satisfaction.',
+           
+        ],
+        'url': 'https://softsynthsoftware.com/',
+    },
+    {
+        'company': 'Codsoft Pvt. Ltd.',
+        'role': 'Frontend Developer Intern',
+        'duration': 'Sept 2023 - Oct 2023',
+        'logo': 'https://media.licdn.com/dms/image/v2/D560BAQHDmIfYA5sY7w/company-logo_200_200/company-logo_200_200/0/1685777115831?e=2147483647&v=beta&t=BrbAEJ0lciXi69uR0VBVOdOGHno8XzclslDmxrVYaNI',
+        'points': [
+            ' Developed & maintained web applications using HTML, CSS, and JavaScript. Collaborated with cross-functional teams to gather requirements and implement design changes.',
+            'Collaborated with cross-functional teams to optimize requirement gathering methods, leading to a 15% decrease in time spent. ',
+            'Technologies Used: HTML, CSS, JavaScript',
+
+        ],
+        'url': 'https://www.codsoft.in/',
+    },
+    {
+        'company': 'Infotrixs Pvt. Ltd.',
+        'role': 'Frontend Developer Intern',
+        'duration': 'Aug 2023 - Sept 2023',
+        'logo': 'https://media.licdn.com/dms/image/v2/D4D0BAQEOgqOJ89to1w/company-logo_200_200/company-logo_200_200/0/1687286850731?e=2147483647&v=beta&t=j8yo5B-DkPPN-zmnGEOfVAJMcNo9DqAGQNuNFCtuS-E',
+        'points': [
+            ' Developed & maintained web applications using HTML, CSS, and JavaScript. Collaborated with cross-functional teams to gather requirements and implement design changes.',
+            'Collaborated with cross-functional teams to optimize requirement gathering methods, leading to a 15% decrease in time spent. ',
+            'Technologies Used: HTML, CSS, JavaScript',
+
+        ],
+        'url': 'https://www.infotrixs.in/',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
