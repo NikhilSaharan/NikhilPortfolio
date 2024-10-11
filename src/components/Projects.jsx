e72567a5a575f68d3bed95c79734f27089665699
Nikhil <140883532+NikhilSaharan@users.noreply.github.com>
2024-10-11 13:40:38 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,30 +2,18 @@
 
 import React from "react";
 
-import Music from '../assets/Music.png'
-import expensetracker from '../assets/expensetracker.png'
-import Color from '../assets/Color .png'
-import CartEase from '../assets/CartEase.png'
-import Footer from './Footer'
+import Music from '../assets/Music.png';
+import expensetracker from '../assets/expensetracker.png';
+import Color from '../assets/Color.png';
+import Techh from '../assets/Techh.png';
+import Footer from './Footer';
 
 const ProjectCard = ({ image, title, description, git, technologies }) => {
     return (
         <div className="max-w-sm sm:max-w-sm md:max-w-sm bg-gray-900 border border-neutral-100 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            {title=='E-Commerce Website' && <a href="#">
-                <img className="w-full rounded-t-lg h-auto object-cover " src={CartEase} alt="" />
-            </a>}
-
-            {title=='Online Music Player' && <a href="#">
-                <img className="w-full rounded-t-lg h-auto object-cover " src={Music} alt="" />
-            </a>}
-
-            {title=='Color Picker' && <a href="#">
-                <img className="w-full rounded-t-lg h-auto object-cover " src={Color} alt="" />
-            </a>}
-
-            {title=='Expense Tracker' && <a href="#">
-                <img className="w-full rounded-t-lg h-auto object-cover " src={expensetracker} alt="" />
-            </a>}
+            <a href="#">
+                <img className="w-full rounded-t-lg h-auto object-cover" src={image} alt={title} />
+            </a>
             <div className="p-4 sm:p-6">
                 <a href="#">
                     <h5 className="text-2xl sm:text-xl md:text-2xl lg:text-3xl font-bold tracking-tight text-white bg-clip-text text-transparent bg-gradient-to-r from-yellow-200 to-pink-500">{title}</h5>
@@ -35,10 +23,7 @@ const ProjectCard = ({ image, title, description, git, technologies }) => {
             <div className='m-2 sm:m-4 lg:m-6 flex justify-between'>
                 <div className='flex flex-wrap gap-2 pl-2'>
                     {technologies.map((tag, index) => (
-                        <p
-                            key={`${index}-${tag}`}
-                            className='text-[14px] text-blue-500'
-                        >
+                        <p key={`${index}-${tag}`} className='text-[14px] text-blue-500'>
                             #{tag}
                         </p>
                     ))}
@@ -48,7 +33,7 @@ const ProjectCard = ({ image, title, description, git, technologies }) => {
         </div>
     );
 };
-  
+
 const Projects = () => {
     return (
         <div className="bg-black">
@@ -59,49 +44,45 @@ const Projects = () => {
                         image={item.image}
                         title={item.title}
                         description={item.description}
-                        links={item.links}
                         git={item.git}
                         technologies={item.technologies}
                     />
                 ))}
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
-}
-
+};
 
 export const project = [
     {
-        title:'E-Commerce Website',
-        description:'I created an E-Commerce website that providing a seamless shopping experience with features like product listings,user-friendly shopping cart,and a streamlined checkout process. Built in using React.js  for a dynamic experience, and Material-UI for a sleek, responsive design.',
-        image: {CartEase},
-        git:"https://cart-ease.vercel.app/",
-        technologies:[ 'React.js, Redux, Material-UI', 'Javascript']
-    },
-    
-    {
-        title:'Online Music Player',
-        description:'Developed a dynamic music player website that allows users to play, pause, skip to the next song, and go back to the previous song, enhancing the user experience for music enthusiasts. Technologies used - HTML, CSS, JavaScript',
-        image: {Music},
-        git:'https://online-music-player-ten.vercel.app//',
-        technologies:['HTML' ,'CSS' , 'Javascript']
+        title: 'TechStore',
+        description: 'I created an E-Commerce website that provides a seamless shopping experience with features like product listings, user-friendly shopping cart, and a streamlined checkout process. Built using React.js for a dynamic experience, and Material-UI for a sleek, responsive design.',
+        image: Techh,
+        git: "https://techstore-xi.vercel.app/",
+        technologies: ['React.js', 'Redux', 'Material-UI', 'JavaScript']
     },
     {
-        title:'Expense Tracker',
-        description:' Developed an interactive Expense Tracker web application aimed at optimizing financial management for users. This project significantly improved financial tracking and user experience. Technologies used - HTML, CSS, JavaScript',
-        image: {expensetracker},
-        git:"https://expense-tracker-xi-lovat.vercel.app/",
-        technologies:[ 'HTML' ,'CSS' , 'Javascript']
+        title: 'Online Music Player',
+        description: 'Developed a dynamic music player website that allows users to play, pause, skip to the next song, and go back to the previous song, enhancing the user experience for music enthusiasts. Technologies used: HTML, CSS, JavaScript.',
+        image: Music,
+        git: 'https://online-music-player-ten.vercel.app/',
+        technologies: ['HTML', 'CSS', 'JavaScript']
     },
+    {
+        title: 'Expense Tracker',
+        description: 'Developed an interactive Expense Tracker web application aimed at optimizing financial management for users. This project significantly improved financial tracking and user experience. Technologies used: HTML, CSS, JavaScript.',
+        image: expensetracker,
+        git: "https://expense-tracker-xi-lovat.vercel.app/",
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        title: 'Color Picker',
+        description: 'Innovated a Color Picker Chrome extension that enables developers to select colors effortlessly while designing user interfaces, improving color selection speed by 80%. Technologies used: HTML, CSS, JavaScript.',
+        image: Color,
+        git: 'https://color-picker-plum-nu.vercel.app/',
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+];
 
-    {
-        title:'Color Picker',
-        description:' Innovated a Color Picker Chrome extension that enables developers to select colors effortlessly while designing user interfaces, improving color selection speed by 80%   Technologies used - HTML, CSS, JavaScript',
-        image: {Color},
-        git:'https://color-picker-plum-nu.vercel.app/',
-        technologies:['HTML' ,'CSS' , 'Javascript']
-    },
-]
-
-export default Projects
+export default Projects;
